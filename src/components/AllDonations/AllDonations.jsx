82b692161daea2f0d6e218c6a960728b675439c4
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-09-27 04:16:18 +0600

    added search functionality

--- src/components/AllDonations/AllDonations.jsx
+++ src/components/AllDonations/AllDonations.jsx
@@ -2,13 +2,26 @@ import PropTypes from "prop-types";
 import DonationCard from "../DonationCard/DonationCard";
 
 const AllDonations = ({ allDonations, searchValue }) => {
-  console.log(searchValue);
+  const searchedCategoryData = allDonations.filter(
+    (donation) => donation.category.toLowerCase() == searchValue.toLowerCase()
+  );
+  console.log(searchedCategoryData);
   return (
     <div className="container mx-auto mt-28 mb-44">
       <div className="grid grid-cols-4 gap-6">
-        {allDonations?.map((donation) => (
-          <DonationCard key={donation.id} donation={donation}></DonationCard>
-        ))}
+        {searchedCategoryData.length > 0
+          ? searchedCategoryData?.map((donation) => (
+              <DonationCard
+                key={donation.id}
+                donation={donation}
+              ></DonationCard>
+            ))
+          : allDonations?.map((donation) => (
+              <DonationCard
+                key={donation.id}
+                donation={donation}
+              ></DonationCard>
+            ))}
       </div>
     </div>
   );
